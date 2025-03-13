210492fda7aa5630a22423ce4033809ab7330788
youyu <129youyu@gmail.com>
2025-03-13 09:21:05 +0800

    用vue完成編輯功能

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,25 +1,43 @@
-<script setup lang="ts">
+<script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import { ref,reactive } from 'vue';
 
-const event=ref('請輸入您的代辦事項');
+const event=ref('');
 const events=reactive(new Array());
+let editText=ref('');
+const showInput=reactive(new Array());
 const addEvent=()=>{
     events.push(event.value);
     event.value='';
-    console.log(event);
+    showInput.push(false);
+   // console.log(showInput);
+}
+const save=(idx)=>{
+    //events.splice(idx,1,editText.value);
+    events[idx]=editText.value;
+    editText.value='';
+    showInput.fill(false);
+}
+const edit=(idx)=>{
+    editText.value=events[idx];
+    showInput.fill(false);
+    showInput[idx]=true;
+    //console.log(showInput);
+    //events.splice(idx,1,editText.value);
+    //editText.value='';
 }
 const del=(idx)=>{
-    events.splice(idx,1)
+    events.splice(idx,1);
 }
+
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="待辦事項">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="flex flex-col mt-6 bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
         <header class="not-has-[nav]:hidden mb-3 w-full max-w-[335px] text-sm lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
                 <Link
@@ -45,37 +63,47 @@ const del=(idx)=>{
                 </template>
             </nav>
         </header>
-        <div class="duration-750 starting:opacity-0 flex w-full items-center justify-center opacity-100 transition-opacity lg:grow">
-            <main class="w-full max-w-[750px] overflow-hidden rounded-lg lg:max-w-4xl flex-col">
-                <div>
-                    <label for="">工作事項</label>
-                    <input class="border border-gray-500 p-2 mx-2 rounded" type="text" name="event" id="event" v-model="event">
-                    <button class="btn btn-lime" @click="addEvent">新增</button>
+        <div class="duration-750 starting:opacity-0 flex w-full justify-center opacity-100 transition-opacity lg:grow">
+            <main class="flex w-full max-w-[750px] content-start flex-wrap overflow-hidden rounded-lg">
+            <div class="w-full  flex justify-center items-center p-4 bg-white rounded-lg">
+                <label for="">工作事項</label>
+                <input class="border border-gray-500 p-2 mx-2"
+                        type="text" name="event" id="event"
+                        v-model="event">
+                <button class="btn btn-lime "
+                        @click="addEvent">
+                    新增
+                </button>
+            </div>
+            <h3 class='text-center w-full text-2xl '>待辦事項清單</h3>
+            <div id="lists" class="w-full p-4">
+                <div  class="w-full flex justify-between"
+                    v-for="event,idx in events" :key="idx">
+                   <div v-if="!showInput[idx]">
+                    {{idx+1}}. {{event}}
+                   </div> 
+                   <div v-else="showInput[idx]">
+                    {{idx+1}}. <input type="text" v-model="editText"  class="border border-gray-500 p-2 mx-2">
+                   </div>
+                   <div>
+                      <button class="btn btn-yellow" v-if="!showInput[idx]"  @click="edit(idx)">編輯</button>
+                      <button class="btn btn-blue" v-if="showInput[idx]" @click="save(idx)">更新</button>
+                      <button class="btn btn-red" @click="del(idx)">刪除</button>
+                   </div>
+
                 </div>
-                <h3 class="text-center text-2xl">代辦事項清單</h3>
-                <div id="lists" class="w-full p-4 flex align-items flex-col">
-                    <p v-for="event,idx in events" :key="idx" class="flex justify-between">
-                        <div>
-                            {{idx+1}}.{{ event }}
-                        </div>
-                        <div>
-                            <button class="btn btn-yellow">編輯</button>
-                            <button class="btn btn-red" @click="del(idx)">刪除</button>
-                        </div>
-                    </p>
-                </div>
+
+            </div>
             </main>
-
         </div>
-
         <div class="h-14.5 hidden lg:block"></div>
     </div>
 </template>
-
 <style scoped>
 .btn{
-    @apply border px-4 py-1 rounded-2xl drop-shadow-lg
+    @apply border  px-4 py-1 rounded-2xl drop-shadow-lg
 }
+
 .btn-lime{
     @apply bg-lime-200 text-lime-800
 }
@@ -84,5 +112,8 @@ const del=(idx)=>{
 }
 .btn-red{
     @apply bg-red-200 text-red-800
+}
+.btn-blue{
+    @apply bg-blue-200 text-blue-800
 }
 </style>
